b73adc117b345792faa2c44e9d61022cc424e499
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-08-10 11:58:16 +0200

    added MyBatis update

--- cockpit-plugin-version-migration/src/main/java/com/camunda/consulting/cockpitPluginVersionMigration/resources/ProcessInstanceMigrationResource.java
+++ cockpit-plugin-version-migration/src/main/java/com/camunda/consulting/cockpitPluginVersionMigration/resources/ProcessInstanceMigrationResource.java
@@ -1,39 +1,24 @@
 package com.camunda.consulting.cockpitPluginVersionMigration.resources;
 
-import java.io.IOException;
+import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response.Status;
 
-import org.camunda.bpm.cockpit.db.QueryParameters;
 import org.camunda.bpm.cockpit.plugin.resource.AbstractCockpitPluginResource;
-import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.impl.ProcessEngineImpl;
-import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.cmd.SetProcessDefinitionVersionCmd;
-import org.camunda.bpm.engine.impl.context.Context;
-import org.camunda.bpm.engine.impl.db.entitymanager.operation.DbEntityOperation;
-import org.camunda.bpm.engine.impl.db.entitymanager.operation.DbOperation;
-import org.camunda.bpm.engine.impl.history.event.HistoricProcessInstanceEventEntity;
-import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
-import org.camunda.bpm.engine.impl.history.event.HistoryEventType;
-import org.camunda.bpm.engine.impl.history.event.HistoryEventTypes;
 import org.camunda.bpm.engine.impl.interceptor.Command;
 import org.camunda.bpm.engine.impl.interceptor.CommandContext;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
-import org.camunda.bpm.engine.rest.exception.InvalidRequestException;
+import org.camunda.bpm.engine.impl.persistence.entity.HistoricProcessInstanceEntity;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 
-import com.camunda.consulting.cockpitPluginVersionMigration.dto.HistoricProcessInstanceDto;
-import com.camunda.consulting.cockpitPluginVersionMigration.dto.ProcessInstanceDto;
 import com.camunda.consulting.cockpitPluginVersionMigration.dto.VersionDto;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ProcessInstanceMigrationResource extends AbstractCockpitPluginResource {
   
@@ -45,102 +30,52 @@ public class ProcessInstanceMigrationResource extends AbstractCockpitPluginResou
 
   @POST
   @Path("{id}")
-  public void migrateProcessInstance(@PathParam("id") String processInstanceId, String payload) {
-    log.fine("process instance id" + processInstanceId + ", payload: " + payload);
-    int newVersion;
-    if (payload != null && payload.length() > 0) {
-      VersionDto versionDto = null;
-      try {
-        versionDto = new ObjectMapper().readValue(payload, VersionDto.class);
-      } catch (JsonParseException e) {
-        throw new InvalidRequestException(Status.BAD_REQUEST, "JsonParseException: " + e.getMessage());
-      } catch (JsonMappingException e) {
-        throw new InvalidRequestException(Status.BAD_REQUEST, "JsonMappingException: " + e.getMessage());
-      } catch (IOException e) {
-        throw new InvalidRequestException(Status.BAD_REQUEST, "IOException: " + e.getMessage());
-      }
-      newVersion = versionDto.getVersion();
-    } else {
-      throw new InvalidRequestException(Status.BAD_REQUEST, "new version is missing");
+  public void migrateProcessInstance(@PathParam("id") final String processInstanceId, VersionDto versionDto) {
+    log.fine("process instance id" + processInstanceId + ", payload: " + versionDto);
+    if (versionDto==null) {
+      throw new IllegalArgumentException("version to migrate to must be given");
     }
-    
+    final int newVersion = versionDto.getVersion();
+   
     log.fine("new version: " + newVersion);
     
-    SetProcessDefinitionVersionCmd command = 
-       new SetProcessDefinitionVersionCmd(processInstanceId, newVersion);
+    SetProcessDefinitionVersionCmd command = new SetProcessDefinitionVersionCmd(processInstanceId, newVersion);
     ProcessEngineImpl processEngineImpl = (ProcessEngineImpl) ProcessEngines.getProcessEngine(engineName);
-    processEngineImpl
-         .getProcessEngineConfiguration()
-         .getCommandExecutorTxRequired().execute(command);
+    processEngineImpl.getProcessEngineConfiguration().getCommandExecutorTxRequired().execute(command);
 
     log.info("setProcessDefinitionVersionCmd finished");
     
-//    // update the historic process instance
-////    log.info("statement mappings: " + processEngineImpl.getProcessEngineConfiguration().getDbSqlSessionFactory().getStatementMappings());
-//    // returns null
-//    
-//    // use either the processEngine command context
-//    processEngineImpl.getProcessEngineConfiguration().getCommandExecutorTxRequired().execute(
-//        
-//        // or the cockpit command context
-////    getCommandExecutor().executeCommand(
-//        // create a new command to update the historic process instance by SQL 
-//        new Command<Void>() {
-//      
-//      String processInstanceId;
-//      int newVersion;
-//      
-//      @Override
-//      public Void execute(CommandContext commandContext) {
-//        log.info(processInstanceId);
-//        // get the new process definition of the process instance, because the transaction is already completed
-//        // try it either by cockpit query service - how to set dynamic parameters to the query?
-//        ProcessInstanceDto processInstanceDto = new ProcessInstanceDto();
-//        processInstanceDto.setProcessInstanceId(processInstanceId);
-////        ProcessInstanceDto processInstance = getQueryService().executeQuery("getProcessDefinition", new QueryParameters<ProcessInstanceDto>());
-//        
-//        // or try it with the query from the plugin-mapping.xml in the process engine commandContext:
-//        //  Mapped Statements collection does not contain value for getProcessDefinition
-////        ProcessInstanceDto processInstance = commandContext.getDbSqlSession().getSqlSession().selectOne("getProcessDefinition", processInstanceDto);
-//        
-//        // or try it with the execution manager. It works only in the commandContext from the process engine
-//        // here we get the new processDefinitionId
-//        ExecutionEntity processInstance = commandContext.getExecutionManager().findExecutionById(processInstanceId);
-//     
-//        log.info("new process definition? " + processInstance.getProcessDefinitionId());
-//        
-//        // try to execute the update from the migrate-historic-processinstance-update.xml
-//        HistoricProcessInstanceDto historicProcessInstanceDto = new HistoricProcessInstanceDto();
-//        historicProcessInstanceDto.setId_(processInstanceId);
-//        historicProcessInstanceDto.setProcessDefinitionId_(processInstance.getProcessDefinitionId());
-//        // ### Error updating database.  Cause: java.lang.IllegalArgumentException: Mapped Statements collection does not contain value for updateHistoricProcessInstance
-//        commandContext.getDbSqlSession().getSqlSession().update("updateHistoricProcessInstance", historicProcessInstanceDto);
-//        return null;
-//      }
-//      
-//      public Command<Void> init(String processInstanceId, int newVersion) {
-//        this.processInstanceId = processInstanceId;
-//        this.newVersion = newVersion;
-//        return this;
-//      }
-//      
-//    }.init(processInstanceId, newVersion));
+    // get the new process definition id from the migrated process instance.
+    // This is the easiest and works because the above transaction is already completed
+    ProcessInstance pi = getProcessEngine().getRuntimeService().createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+    final String processDefinitionId = pi.getProcessDefinitionId();
     
-    // tried to fire the history event manually, butt didn't work either
-//  String newProcessDefinitionId = ;
-//  
-//  ProcessEngineConfigurationImpl processEngineConfiguration = (ProcessEngineConfigurationImpl) getProcessEngine().getProcessEngineConfiguration();
-//  
-//  HistoryEvent historyEvent = new HistoryEvent();
-//  historyEvent.setEventType(HistoryEventTypes.TASK_INSTANCE_COMPLETE.getEntityType());
-//  processEngineConfiguration.getHistoryEventHandler().handleEvent(historyEvent);
-//  processEngineConfiguration.getClass();
-//  DbEntityOperation operation = new DbEntityOperation();
-//  HistoricProcessInstanceEventEntity historicProcessInstanceEntity = new HistoricProcessInstanceEventEntity();
-//  historicProcessInstanceEntity.setId(processInstanceId);
-//  historicProcessInstanceEntity.setProcessDefinitionId(newProcessDefinitionId);
-//  operation.setEntity(historicProcessInstanceEntity);
-//  Context.getCommandContext().getDbSqlSession().executeDbOperation(operation);
+    // now do a SQL update via MyBatis
+    getCommandExecutor().executeCommand(new Command<Void>() {
+      
+      @Override
+      public Void execute(CommandContext commandContext) {
+        log.info("Now tweaking histor of " + processInstanceId);
+        
+        HistoricProcessInstanceEntity processInstanceDto = new HistoricProcessInstanceEntity();
+        processInstanceDto.setProcessInstanceId(processInstanceId);
+        processInstanceDto.setProcessDefinitionId(processDefinitionId);
+        
+        try {       
+          // execute the update from the migrate-historic-processinstance-update.xml
+          commandContext.getDbSqlSession().getSqlSession().update("cockpit.version-migration.updateHistoricProcessInstance", processInstanceDto);
+          
+          log.info("History tweaked successfully");
+        }
+        catch (Throwable ex) {
+          log.log(Level.INFO, "Could not tweak history due to exception", ex);
+          // Classloading problems!
+          // caused by: java.lang.NoClassDefFoundError: org/apache/ibatis/session/SqlSession
+          // at com.camunda.consulting.cockpitPluginVersionMigration.resources.ProcessInstanceMigrationResource$1.execute(ProcessInstanceMigrationResource.java:63) [cockpit-plugin-version-migration.jar:]
+        }
+        return null;
+      }
+    });
     
   }
 }
--- cockpit-plugin-version-migration/src/main/resources/plugin-webapp/version-migration/app/plugin.js
+++ cockpit-plugin-version-migration/src/main/resources/plugin-webapp/version-migration/app/plugin.js
@@ -5,27 +5,21 @@ define(['angular'], function(angular) {
   var loadProcessDefinition = function(processInstance, $scope, $http, Uri) {
     $http.get(Uri.appUri("engine://engine/:engine/process-definition/" + processInstance.definitionId))
     .success(function(processDefinition) {
-      console.log(processDefinition);
       $scope.processDefinition = processDefinition;
     });
   };
   
   var VersionMigrationController = function($scope, $http, Uri) {
 
-    console.log('Process instance id', $scope.processInstance.id);
-    console.log('process definition id', $scope.processInstance.definitionId);
-    
     loadProcessDefinition($scope.processInstance, $scope, $http, Uri);
 
     $scope.migrateVersion = function() {
-      console.log('migrate the version to ' + $scope.newVersion.version + ' now.');
       $http.post(Uri.appUri("plugin://version-migration/:engine/process-instance-migration/" + 
           $scope.processInstance.id), $scope.newVersion)
         .then(function() {
           // refresh all views?
           // $scope.processData.set('filter', angular.extend({}, $scope.filter));
           window.location.reload();
-          console.log('version updated');
         });
     }
   };
@@ -44,7 +38,6 @@ define(['angular'], function(angular) {
   };
 
   Configuration.$inject = ['ViewsProvider'];
-
   
   ngModule.config(Configuration);
   ngModule.controller('VersionMigrationController', [ '$scope' ]);
